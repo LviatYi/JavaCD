caf3cfa4add4428dcacfe0c1ca197c4fd9b75b9c
LviatYi <LviatYi@qq.com>
2021-06-05 11:08:59 +0800

    update Status

---
+++ src/LoginStatus.java
@@ -0,0 +1,27 @@
+/**
+ * 登录状态
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @enumName LoginStatus
+ * @date 2021/6/5
+ */
+enum LoginStatus
+{
+    /**
+     * 登录成功
+     */
+    SUCCESS,
+    /**
+     * 网络未连接
+     */
+    CONNECTION_FAILED,
+    /**
+     * 账号不存在
+     */
+    ID_NOT_EXIST,
+    /**
+     * 密码错误
+     */
+    PASSWORD_ERROR
+}

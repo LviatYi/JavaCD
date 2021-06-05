2ef9fee62b46c77e4f20ad9eb98371b6a0a0e970
May_bebe <897159980@qq.com>
2021-06-05 12:15:58 +0800

    delete getKey and update EncryptionImpl

--- src/Encryption.java
+++ src/Encryption.java
@@ -14,25 +14,18 @@ public interface Encryption {
     String encryptPassword(String password);
 
     /**
-     * 对聊天内容加密，确保每一聊天内容文本的加密解密使用同一密钥
+     * 对聊天内容加密
      * @param content 聊天内容
-     * @param key 密钥
      * @return 加密后的聊天内容
      */
-    String encryptContent(String content, String key);
+    String encryptContent(String content);
 
     /**
-     * 对加密后的聊天内容解密，确保每一聊天内容文本的加密解密使用同一密钥
-     * @param content 加密后的聊天内容
-     * @param key 密钥
+     * 对加密后的聊天内容解密
+     * @param keyContent 密钥和加密后的聊天内容的合并字符串
      * @return 聊天内容
      */
-    String decryptContent(String content, String key);
+    String decryptContent(String keyContent);
 
-    /**
-     * 获取密钥
-     * @param keyLength 密钥长度
-     * @return 密钥
-     */
-    String getKey(int keyLength);
+
 }

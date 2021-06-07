ed84bbae24af5eb4e86098c84418d1693aacfa13
Sticker <sjl11081430>
2021-06-07 08:41:23 +0800

    123

--- src/DataBase/Connect.java
+++ src/DataBase/Connect.java
@@ -32,17 +32,7 @@ public class Connect<date> implements Database
          */
         PASSWORD_ERROR
     }
-   public enum RegisterStatus
-            {
-                /**
-                 * 注册成功
-                 */
-                SUCCESS,
-                /**
-                 * 网络未连接
-                 */
-                CONNECTION_FAILED
-            }
+
     static {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -55,9 +45,9 @@ public class Connect<date> implements Database
      * 加载驱动程序
      */
     public Connection getConnection() {
-        String url = "jdbc:sqlserver://127.0.0.1:1433;DatabaseName=JavaCD";
+        String url = "jdbc:sqlserver://127.0.0.1:1433;DatabaseName=b";
         try {
-            con = DriverManager.getConnection(url, "SA", "JavaCD2021");
+            con = DriverManager.getConnection(url, "sa", "111111");
             sta = con.createStatement();
             //System.out.println("连接成功");
         } catch (SQLException e) {
@@ -79,7 +69,19 @@ public class Connect<date> implements Database
        //System.out.println("建立表成功");
 
    }*/
+   int sui(){
+       int num= (int) Math.floor(Math.random()*11);
+       while(num<6||num>10)
+           num= (int) Math.floor(Math.random()*11);
 
+
+
+       //System.out.println(num);
+       int a= (int) Math.pow(10,(num-1));
+       int random6=(int)((Math.random()*9+1)*a);
+       return random6;
+
+   }
 
     public LoginStatus LogIn(String id,String password)   //登录  查询name和password是否与数据库匹配
     {       con=getConnection();
@@ -87,23 +89,25 @@ public class Connect<date> implements Database
             String sql = "SELECT * FROM userInfo where id='" + id + "'";//and password ='" + password + "'"\;
             ResultSet rs = st.executeQuery(sql);
             while(rs.next()) {
-                System.out.println(rs.getString(2));
-                System.out.println(password);
+                //System.out.println(rs.getString(2));
+                //System.out.println(password);
                 String m1=rs.getString(2);
                 if(password.equals(m1)) return LoginStatus.SUCCESS;
                else{ return LoginStatus.PASSWORD_ERROR;}}
         }catch(SQLException e){e.printStackTrace();}return LoginStatus.ID_NOT_EXIST;}
 
-    public RegisterStatus Register(String password,String name)//注册   添加id，密码，账号
+    public int Register(String password, String name)//注册   添加id，密码，账号
     {
         con=getConnection();
         try{String sql="insert into userInfo (password,name) values(?,?)";
             PreparedStatement st=con.prepareStatement(sql);
-            st.setString(2,password);
-            st.setString(3,name);
+            st.setString(1,password);
+            st.setString(2,name);
             int rs=st.executeUpdate();
-            return RegisterStatus.SUCCESS;
-            }catch (SQLException e){e.printStackTrace();return null;}
+            int a=sui();
+            return a;
+            }catch (SQLException e){e.printStackTrace();}
+        return 0;
     }
 
 
@@ -293,8 +297,9 @@ public List<Message> GetMessage(String id) {
             System.out.println("删除好友成功");}catch(SQLException e){e.printStackTrace();}
     }
 
-    //public static void main(String[] args) throws SQLException {//测试部分
-     //DataBase.Connect db=new DataBase.Connect();
+    public static void main(String[] args) throws SQLException {//测试部分
+     DataBase.Connect db=new DataBase.Connect();
+        //System.out.println(db.Register("11","11"));
      //String id="290111";
      //String password="qwerasdf";
      //String name="jdasda";
@@ -306,7 +311,7 @@ public List<Message> GetMessage(String id) {
         //db.GetMessage(time);
        // System.out.println(Tool.getTime());
 
-    }
+    }}
 
 
 

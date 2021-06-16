8823391530e0687b4b621de17f3f88e6f62672cf
孔祥翔 <1055020778@qq.com>
2021-06-16 16:47:48 +0800

    爆改客户端

--- src/Socket/Client/ClientCommunication.java
+++ src/Socket/Client/ClientCommunication.java
@@ -194,15 +194,14 @@ public class ClientCommunication implements Client {
     public FriendInfo findFriend(String userID) {
         try {
             Socket socket = new Socket("127.0.0.1", 9000);
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            InputStream dis = new DataInputStream(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dis));
             DataPacket mes = new DataPacket();
             mes.type = DataPacket.transportType.FIND_FRIEND_INFO;
             mes.id = userID;
-            dos.writeUTF(JSONObject.toJSONString(mes));
-            dos.flush();
+            sendToServer(JSONObject.toJSONString(mes),socket);
             while (true) {
-                DataPacket dp = JSON.parseObject(dis.readUTF(), DataPacket.class);
+                DataPacket dp = JSON.parseObject(bufferedReader.readLine(), DataPacket.class);
                 if (dp.type == DataPacket.transportType.FIND_FRIEND_INFO && dp.systemTip == true) {
                     return dp.friendInfo;
                 }
@@ -230,15 +229,14 @@ public class ClientCommunication implements Client {
     public ChatroomInfo findChatRoomInfo(String chatRoomID) {
         try {
             Socket socket = new Socket("127.0.0.1", 9000);
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            InputStream dis = new DataInputStream(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dis));
             DataPacket mes = new DataPacket();
             mes.type = DataPacket.transportType.FIND_CHATROOM_INFO_THROUGH_ID;
             mes.chatRoomID = chatRoomID;
-            dos.writeUTF(JSONObject.toJSONString(mes));
-            dos.flush();
+            sendToServer(JSONObject.toJSONString(mes),socket);
             while (true) {
-                DataPacket dp = JSON.parseObject(dis.readUTF(), DataPacket.class);
+                DataPacket dp = JSON.parseObject(bufferedReader.readLine(), DataPacket.class);
                 if (dp.type == DataPacket.transportType.FIND_CHATROOM_INFO_THROUGH_ID) {
                     return dp.chatRoomInfo;
                 }

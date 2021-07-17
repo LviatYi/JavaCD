64b6fb246a3e039da3b3c40a2bfaa26321152709
LviatYi <54713360+LviatYi@users.noreply.github.com>
2021-07-17 21:17:52 +0800

    update Database

--- src/DataBase/DatabaseControl.java
+++ src/DataBase/DatabaseControl.java
@@ -4,53 +4,50 @@ import Chatroom.ChatManager.Message;
 import Chatroom.ChatManager.MessageList;
 import Chatroom.ChatroomManager.ChatroomInfo;
 import Chatroom.ChatroomManager.ChatroomList;
-import Chatroom.ChatroomManager.ChatroomManager;
 import Chatroom.FriendManager.FriendInfo;
 import Chatroom.FriendManager.FriendList;
-import Socket.tools.DataPacket;
 import Status.ChatroomStatus;
 import Status.LoginStatus;
 
 import java.util.Date;
-import java.util.List;
-
-
-
 
 /**
- * 向 Socket_Server 提供数据库控制函数
- * @author topkang
+ * 向 Socket_Server
+ *
  * @author LviatYi
- * @author stiker
  * @date 2021/6/15
  */
 public interface DatabaseControl {
 
- /**
-  * 通过ID找姓名
-  * @param ID 人ID
-  * @return 返回姓名
-  */
-    String findNameThroughID(String ID);
     /**
-     *
-     *@return 返回聊天室Id
-     */
-     String returnChatRoomId();
-
-
-/**
- *
- * 通过一个人的ID查找所有信息
- * @param Id 用户 Id
- * @return 好友信息
- */
-   FriendInfo returnUser(String Id);
-
-
-    /**
+     * 通过 ID 找 UserName
      *
      * @param id 用户 ID
+     * @return 返回姓名
+     */
+    String getUserName(String id);
+
+
+    /**
+     * 通过一个人的ID查找所有信息
+     *
+     * @param id 用户 id
+     * @return 好友信息
+     */
+    FriendInfo getUser(String id);
+
+    /**
+     * 返回一个空的聊天室 ID
+     *
+     * @return 返回聊天室 Id
+     */
+    String getVoidChatroomId();
+
+
+    /**
+     * 尝试登录
+     *
+     * @param id       用户 ID
      * @param password 用户 Password
      * @return 登录状态
      */
@@ -58,39 +55,40 @@ public interface DatabaseControl {
 
 
     /**
-     * 注册 添加数据至数据库
+     * 尝试注册
+     * 注册成功后将添加新的用户信息至数据库
      *
      * @param password 用户密码
-     * @param name 用户昵称
-     * @return 录入信息并返回一个 6-10 的随机数字 id（不重复）
+     * @param name     用户昵称
+     * @return 录入信息并返回分配的随机 id
      */
     String register(String password, String name);
 
     /**
-     * //* 修改数据库中的密码
+     * 修改密码
      *
      * @param id           用户id
-     * @param password_now 修改后的密码
+     * @param passwordNew 修改后的密码
      * @return 密码修改状态
      */
-    boolean modifyPassword(String id, String password_now);
+    boolean modifyPassword(String id, String passwordNew);
 
     /**
      * 修改昵称
      *
      * @param id       用户id
-     * @param name_now 修改后的昵称
+     * @param nameNow 修改后的昵称
      * @return 姓名更改状态
      */
-    boolean modifyName(String id, String name_now);
+    boolean modifyName(String id, String nameNow);
 
     /**
      * 添加 Message 至数据库
      *
      * @param chatroomId Message 所在的聊天室 ID
-     * @param senderId 发送者 ID
-     * @param content 内容
-     * @param sendTime 客户端发送时间
+     * @param senderId   发送者 ID
+     * @param content    内容
+     * @param sendTime   客户端发送时间
      * @return 记录状态
      */
     boolean recordMessage(String senderId, String content, String chatroomId, Date sendTime);
@@ -106,7 +104,7 @@ public interface DatabaseControl {
     /**
      * 返回指定聊天室的历史消息
      *
-     * @param chatroomId 聊天室ID
+     * @param chatroomId 聊天室 ID
      * @return 聊天室历史消息 MessageList
      */
     MessageList getHistoryMessage(String chatroomId);
@@ -114,9 +112,11 @@ public interface DatabaseControl {
     /**
      * 创建聊天室
      *
+     * @param chatroomInfo 创建聊天室 Info
+     * @param creatorInfo  创建者 Info
      * @return 返回完整的 ChatroomInfo
      */
-    ChatroomInfo createChatroom(ChatroomInfo chatroomInfo,FriendInfo creatorInfo);
+    ChatroomInfo createChatroom(ChatroomInfo chatroomInfo, FriendInfo creatorInfo);
 
     /**
      * 删除指定聊天室
@@ -129,7 +129,7 @@ public interface DatabaseControl {
     /**
      * 退群
      *
-     * @param userId 退群人 ID
+     * @param userId     退群人 ID
      * @param chatroomId 退出聊天室 ID
      * @return 退出状态
      */
@@ -138,7 +138,7 @@ public interface DatabaseControl {
     /**
      * 添加好友关系
      *
-     * @param id 添加人 ID
+     * @param id        添加人 ID
      * @param id_friend 被添加人 ID
      * @return 添加状态
      */
@@ -147,17 +147,17 @@ public interface DatabaseControl {
     /**
      * 添加指定 用户 到指定聊天室.
      *
-     * @param id 进入者 ID
+     * @param id         进入者 ID
      * @param chatroomId 添加进的聊天室
      * @return 加入状态.
-     *   若加入聊天室为 私人聊天室 则返回 false。
+     * 若加入聊天室为 私人聊天室 则返回 false。
      */
     ChatroomStatus joinChatroom(String id, String chatroomId);
 
     /**
      * 删除好友关系
      *
-     * @param id 自己ID
+     * @param id        自己ID
      * @param id_friend 好友ID
      * @return 删除状态
      */
@@ -181,13 +181,15 @@ public interface DatabaseControl {
 
     /**
      * 根据指定 ID 返回聊天室信息.
+     *
      * @param chatroomID 指定的聊天室 ID
      * @return ChatroomInfo
      */
     ChatroomInfo getChatroomInfo(String chatroomID);
 
     /**
-     *  根据指定 好友关系返回聊天室 Info
+     * 根据指定 好友关系返回聊天室 Info
+     *
      * @param userId1 用户 1 ID
      * @param userId2 用户 2 ID
      * @return ChatroomList
@@ -195,7 +197,8 @@ public interface DatabaseControl {
     ChatroomList getChatroomInfo(String userId1, String userId2);
 
     /**
-     *  根据指定 好友关系返回私有聊天室 Info
+     * 根据指定 好友关系返回私有聊天室 Info
+     *
      * @param userId1 用户 1 ID
      * @param userId2 用户 2 ID
      * @return ChatroomInfo
